d136c9a09bd99fb5ce17d5f2a399280fd8d30530
Tue Anh <pta271996@gmail.com>
2017-08-20 10:37:38 +0700

    thêm đối tượng zombie nhảy dù

---
+++ Assets/Resources/Scripts/FlyingZombieController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingZombieController : MonoBehaviour {
+
+    public float dropTime;
+    public Transform parachutePos;
+    public GameObject parachute;
+    public GameObject zombieFam5;
+
+	// Use this for initialization
+	void Start () 
+    {
+        Invoke("Land", dropTime);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+		
+	}
+
+    void Land()
+    {
+        Instantiate(parachute, parachutePos.position, parachute.transform.rotation);
+        Instantiate(zombieFam5, transform.position, zombieFam5.transform.rotation);
+        Destroy(gameObject);
+    }
+}
