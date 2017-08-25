7a94ffdf7d89086255baad186a4a38f850bab63b
Tue Anh <pta271996@gmail.com>
2017-08-25 10:59:18 +0700

    tạo animation cho đối tượng zombie mower

---
+++ Assets/Resources/Scripts/ZombieMowerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieMowerController : MonoBehaviour {
+
+    public float speed;
+    public Animator myAnim;
+    private bool isDead;
+
+	// Use this for initialization
+	void Start ()
+    {
+        isDead = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (!isDead)
+            Move();
+	}
+
+    void Move()
+    {
+        transform.position -= Vector3.right * speed * Time.deltaTime;
+    }
+}
