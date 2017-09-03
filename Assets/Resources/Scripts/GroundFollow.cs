2a6553015a4c91adfb2df3710dbb23a289945162
Tue Anh <pta271996@gmail.com>
2017-09-03 15:02:07 +0700

    hiệu ứng vỏ đạn final

---
+++ Assets/Resources/Scripts/GroundFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundFollow : MonoBehaviour {
+
+    public Transform shell;
+    public float speed;
+
+
+	// Update is called once per frame
+	void Update () 
+    {
+        if(shell)
+            Follow();
+	}
+
+    void Follow()
+    {
+        Vector3 NewPos = new Vector3(shell.position.x, transform.position.y, 0);
+        this.transform.position = Vector3.Lerp(transform.position, NewPos, Time.deltaTime*speed); 
+    }
+}
---
+++ Assets/Resources/Scripts/ShellController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShellController : MonoBehaviour {
+
+    public Rigidbody2D myRigidbody;
+    public float forceMin;
+    public float forceMax;
+
+    float lifetime = 3.0f;
+    float fadetime = 1.5f;
+
+	// Use this for initialization
+	void Start () 
+    {
+        float force = Random.Range(forceMin, forceMax);
+        myRigidbody.AddForce(transform.right * force);
+        myRigidbody.AddTorque(0.01f*force);
+
+        StartCoroutine(Fade());
+	}
+
+    IEnumerator Fade()
+    {
+        yield return new WaitForSeconds(lifetime);
+
+        float percent = 0;
+        float fadeSpeed = 1 / fadetime;
+        Material mat = GetComponent<Renderer>().material;
+        Color initialColour = mat.color;
+
+        while (percent < 1)
+        {
+            percent += Time.deltaTime * fadeSpeed;
+            mat.color = Color.Lerp(initialColour, Color.clear, percent);
+            yield return null;
+        }
+
+        Destroy(gameObject);
+    }
+}
