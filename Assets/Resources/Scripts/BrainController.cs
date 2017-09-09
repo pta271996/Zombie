57bad2cbe4e109a7381f31f31334726bd0c4037d
Tue Anh <pta271996@gmail.com>
2017-09-10 00:55:56 +0700

    thêm hiệu ứng chất nhầy + đạn não

---
+++ Assets/Resources/Scripts/BrainController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrainController : MonoBehaviour {
+
+    public float speed;
+    public float angle;
+    public Rigidbody2D myRB;
+    public GameObject liquidSplash;
+
+    private Quaternion firstRotation;
+
+	// Use this for initialization
+	void Start () 
+    {
+        var rotationVector = transform.rotation.eulerAngles;
+        rotationVector.z = angle;
+        transform.rotation = Quaternion.Euler(rotationVector);
+        firstRotation = transform.rotation;
+
+        myRB.AddTorque(Random.Range(120.0f,160.0f));
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        Move();
+	}
+
+    void Move()
+    {
+        Vector3 pos = transform.position;
+        Vector3 velocity = new Vector3(speed * Time.deltaTime, 0, 0);
+        pos += firstRotation * velocity;
+        transform.position = pos;
+    }
+
+    void OnTriggerEnter2D(Collider2D otherColl)
+    {
+        if(otherColl.tag == "zombie")
+        {
+            Instantiate(liquidSplash, transform.position, liquidSplash.transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Resources/Scripts/LiquidController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiquidController : MonoBehaviour {
+
+    public GameObject bloodSplatter;
+
+	// Use this for initialization
+	void Start () 
+    {
+        Instantiate(bloodSplatter, new Vector3(transform.position.x + Random.Range(1.25f, 1.5f), transform.position.y - 0.55f, 0.0f), bloodSplatter.transform.rotation);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
